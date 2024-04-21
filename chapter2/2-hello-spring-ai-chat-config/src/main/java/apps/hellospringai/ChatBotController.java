0ede62fbf9e8a9b7c78ebc3cae8ea1243ec38abc
Muthukumaran Navaneethakrishnan <muthuishere@gmail.com>
2024-04-21 14:36:23 +0530

    updates

--- chapter2/2-hello-spring-ai-chat-config/src/main/java/apps/hellospringai/ChatBotController.java
+++ chapter2/2-hello-spring-ai-chat-config/src/main/java/apps/hellospringai/ChatBotController.java
@@ -1,7 +1,11 @@
 package apps.hellospringai;
 
 
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatClient;
+import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatOptions;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +22,27 @@ public class ChatBotController {
     }
 
 
+//    @PostMapping("/api/chat")
+//    public ChatBotResponse askQuestion(@RequestBody ChatBotRequest chatBotRequest) {
+//
+//        String answer = vertexAiGeminiChatClient.call(chatBotRequest.question());
+//        return new ChatBotResponse(chatBotRequest.question(), answer);
+//    }
+
+
     @PostMapping("/api/chat")
     public ChatBotResponse askQuestion(@RequestBody ChatBotRequest chatBotRequest) {
 
-        String answer = vertexAiGeminiChatClient.call(chatBotRequest.question());
+        String question = chatBotRequest.question();
+        ChatOptions options = VertexAiGeminiChatOptions.builder()
+                .withMaxOutputTokens(100)
+                .withTemperature(0.5f)
+                .build();
+        Prompt prompt = new Prompt(question, options);
+
+        ChatResponse chatResponse = vertexAiGeminiChatClient.call(prompt);
+
+        String answer = chatResponse.getResult().getOutput().getContent();
         return new ChatBotResponse(chatBotRequest.question(), answer);
     }
 
