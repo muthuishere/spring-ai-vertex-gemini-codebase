c19d97919205fc5d4a4aff2a45fee2f7a5c6e681
Muthukumaran Navaneethakrishnan <muthuishere@gmail.com>
2024-06-09 21:47:08 +0530

    using embeddings

---
+++ chapter9/2-vector-db/src/test/java/apps/springai/embeddings/TextEmbeddingClientIntegrationTest.java
@@ -0,0 +1,39 @@
+package apps.springai.embeddings;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class TextEmbeddingClientIntegrationTest {
+
+
+    @Autowired
+    TextEmbeddingClient textEmbeddingClient;
+
+    Logger log = LoggerFactory.getLogger(TextEmbeddingClientIntegrationTest.class);
+
+    @Test
+    void testEmbeddingReturnsExpectedSize() {
+
+
+        String question = "What is the average rating for all products in the brand apple?";
+        List<Double> embeddings = textEmbeddingClient.embed(question);
+        assertNotNull(embeddings);
+
+        assertEquals(768, embeddings.size());
+
+        log.info(embeddings.toString());
+        log.info("size " + embeddings.size());
+
+    }
+
+
+}
