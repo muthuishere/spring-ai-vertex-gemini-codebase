ecded2d48c80d5719aeeceb5312cf1e70a559281
Shaamam <110484017+Shaamam@users.noreply.github.com>
2025-05-25 11:03:55 +0530

    update version to 1.0.0, refactor chat response handling, and integrate chat client for SQL tool execution

--- chapter8/1-embeddings/src/main/java/apps/unstructured/products/ProductController.java
+++ chapter8/1-embeddings/src/main/java/apps/unstructured/products/ProductController.java
@@ -2,23 +2,20 @@ package apps.unstructured.products;
 
 import apps.unstructured.ChatBotRequest;
 import apps.unstructured.ChatBotResponse;
-import apps.unstructured.products.sql.QueryRequest;
-import apps.unstructured.products.sql.QueryResponse;
+import apps.unstructured.tools.SQLTools;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.model.function.FunctionCallbackWrapper;
 import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
-import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatOptions;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -30,40 +27,44 @@ public class ProductController {
     private final ProductRepository productRepository;
     private final ProductAiService productAiService;
     private final VertexAiGeminiChatModel vertexAiGeminiChatModel;
-    private final   FunctionCallbackWrapper<QueryRequest, QueryResponse> llmSqlQueryExecutorFunction;
+    private final JdbcTemplate jdbcTemplate;
+
+    private ChatClient chatClient;
+
+    @PostConstruct
+    void setup() {
+        chatClient = ChatClient.builder(vertexAiGeminiChatModel)
+                .defaultSystem("You are a smart helpful assistant that can answer user questions, generate SQL query/queries for that user question, execute those SQL query/queries and respond in human understandable language.")
+                .defaultTools(new SQLTools(jdbcTemplate))
+                .build();
+    }
 
 
     @PostMapping("/chat-with-inventory")
     public ChatBotResponse chatWithInventory(@RequestBody ChatBotRequest chatBotRequest) {
 
         String question = chatBotRequest.question();
-        ;
 
-        String context = productAiService.readFromClasspath("generate-sql-function-prompt.txt");
+        String context = productAiService.readFromClasspath("generate-sql-tool-calling-prompt.txt");
+        String assistantMessage = productAiService.readFromClasspath("generate-sql-tool-calling-prompt.txt");
 
 
         var messages = new ArrayList<Message>();
-        messages.add(new SystemMessage(context));
+        messages.add(new UserMessage(context));
+        messages.add(new AssistantMessage(assistantMessage));
         messages.add(new UserMessage(question));
 
 
-        ChatOptions options = VertexAiGeminiChatOptions.builder()
-                .withFunctionCallbacks(List.of(llmSqlQueryExecutorFunction))
-                .build();
+        Prompt prompt = new Prompt(messages);
 
-        Prompt prompt = new Prompt(messages,options);
         // call the chat client
-        ChatResponse chatResponse = vertexAiGeminiChatModel.call(prompt);
+        String answer = chatClient.prompt(prompt)
+                .call()
+                .content();
 
-        log.info("Response: {}", chatResponse);
-        // get the answer
-        String answer = chatResponse.getResult().getOutput().getContent();
-
-
-
+        log.info("Response: {}", answer);
 
         return new ChatBotResponse(question, answer);
-
     }
 
 
